816fdd20538b707745d89f16be91102cce570b0e
Ruari Day <ruariday@gmail.com>
2020-03-12 09:04:26 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,18 +2,16 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague",
-        :age => "53"
+        #your key/value pairs here
         },
       :matriarch => {
-        :name => "Lady Montague",
-        :age => "54"
-      },
+        #your key/value pairs here
+        },
       :hero => {
         #your key/value pairs here
         },
       :hero_friends => []
-  , 
+   }, 
    :capulet => {
       :patriarch => {
         #your key/value pairs here
